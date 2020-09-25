1bd895313b285b346d6d08f82bcaa9827c1e53ac
CharliePhillips <cp865617@gmail.com>
2020-09-25 12:21:03 -0400

    repo updated via automatic bat file

--- CarInstance.gd
+++ CarInstance.gd
@@ -12,26 +12,30 @@ var fric = 0
 var laneOut = false
 var lane = "straight"
 var go = true
-var initRot = get_rotation_degrees()
 var rotSpeed = 5
+var see = false
+var initRot = get_rotation_degrees()
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initRot = get_rotation_degrees()
 	pass # Replace with function body.
 
 func _on_rightLane_body_entered(body):
 	if(body is KinematicBody2D):
 		body.lane = "right"
-	print(body, "entered")
+	#print(body, "entered")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(go == true):
-		if(abs(speedXY) < maxSpeed):
+		if(abs(speedXY) < maxSpeed && see != true && see == false):
 			speedXY += accel
+		if(see == true && speedXY > 0):
+			speedXY -= accel
 		if(lane == "right" && laneOut == true && get_rotation_degrees() < initRot + 90):
+			print(initRot)
 			set_rotation_degrees(get_rotation_degrees() + rotSpeed)
-			print('x', speed.x)
-			print('y', speed.y)
+			print(get_rotation_degrees())
 		speed.y = speedXY * -cos(get_rotation())
 		speed.x = speedXY * sin(get_rotation())
 	move_and_slide(speed)
@@ -39,10 +43,11 @@ func _process(delta):
 
 
 func _on_rightLane_body_exited(body):
-	if(body is KinematicBody2D):
+	print('exit')
+	if body is KinematicBody2D:
 		body.laneOut = true
 		print(lane)
-		print(laneOut)
+		print(body, laneOut)
 	pass # Replace with function body.
 	
 	
@@ -52,4 +57,18 @@ func _on_rightLane_body_exited(body):
 func _on_Area2D_body_entered(body):
 	if(body != self):
 		queue_free()
+	pass # Replace with function body.
+
+
+func _on_Eyeline_body_entered(body):
+	if body is KinematicBody2D && body != self:
+		see = true
+		#print("see ", body)
+	pass # Replace with function body.
+
+
+func _on_Eyeline_body_exited(body):
+	if body is KinematicBody2D && body != self:
+		see = false
+		#print("no see ", body)
 	pass # Replace with function body.
